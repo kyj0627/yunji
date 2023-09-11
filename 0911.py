48dfc5af6e562f5a5be2acf40005583335d499e3
kyj0627 <audry0627@naver.com>
2023-09-11 18:09:05 +0900

    0911.py

---
+++ 0911.py
@@ -0,0 +1,120 @@
+""" my_set = set([5,8,3,7,1,"h"])
+#set_tmp = set("hello")
+#print(set_tmp)
+set_item={7,8,4,2,"h"} """
+
+#print(my_set | set_item)
+#print(my_set.union(set_item))
+#print(my_set - set_item)
+#print(my_set.difference(set_item))
+#print(my_set & set_item)
+#print(my_set.interection(set_item))
+
+#my_set = {5,8,3,7,1,"h"}
+""" my_set.add(9)
+print(my_set) """
+""" my_set.update([5,9,7,4])
+print(my_set)
+ """
+""" my_set.clear()
+print(my_set) """
+""" my_set.remove(5)
+print(my_set)
+my_set.discard(2)
+print(my_set) """
+""" set_item = {7,8,4,2,"h"}
+my_set.difference_update(set_item)
+print(my_set) """
+
+""" my_int=10
+print(my_int+10)
+my_str=str(my_int)
+
+print(my_str+"hello") """
+
+
+
+""" x=10
+y=3
+print(x + y)    
+print(x - y)    
+print(x * y)    
+print(x / y)    
+print(x % y)    
+print(x // y)   
+print(x ** y)  """  
+
+""" a= 0 
+print(a)
+a+=2
+print(a)
+a-=1
+print(a)
+a*=4
+print(a)
+a/=2
+print(a)
+a**=3
+print(a) """
+
+""" x=10
+y=4
+print(x > y)    
+print(x < y)    
+print(x >= y)   
+print(x <= y)   
+print(x == y)   
+print(x != y)   """ 
+
+""" a=1
+b=0
+print(a and b)  #and는 둘이 같아야 1 혹은 True
+print(a or b)   #or은 둘 중 하나만 1(True)이어도 1 뜨거나 True
+print(not a)    
+print(not b)   
+
+x=True
+y=False 
+print(x and y)  
+print(x or y)   
+print(not x)    
+print(not y)   """
+
+""" x=10
+y=3
+
+print(x & y)   
+print(x | y)   
+print(x ^ y)   
+print(~x)      
+print(x << 2)  
+print(x >> 2) """
+
+""" my_list = [9,4,3,7,8,'hi']
+print(4 in my_list)
+print(2 in my_list)
+print(2 not in my_list)
+
+my_dic = {"key1 : "v1" , "k2" : "v2"} """
+
+""" x = [1, 2, 3]
+y = [1, 2, 3]
+z = x
+
+print(x is y)  
+print(x is z)  
+print(x is not y)   """
+
+""" a=5
+b=5
+
+print(a is b)  
+print(a is not b) """
+
+""" 3 == 3.0
+print(3 is 5.0)
+ """
+""" a = [3 , 5, 1]
+b = a
+print(a is b)  
+print(a is not b) """
