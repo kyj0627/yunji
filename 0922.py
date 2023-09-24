acc3a59cea208055f26f0c043ec34e2d2747fae5
kyj0627 <143999633+kyj0627@users.noreply.github.com>
2023-09-24 22:17:56 +0900

    Update 0922.py

--- 0922.py
+++ 0922.py
@@ -1,15 +1,1 @@
-def add(a, b):
-    print("a + b: ", a + b)
-    return a + b
 
-def sub(a, b):
-    print("a - b :", a -b)
-    return a - b
-
-def mul(a, b):
-    print("a * b :", a * b)
-    return a * b
-
-def alv(a, b):
-    print('a / b:', a / b)
-    return a / b
